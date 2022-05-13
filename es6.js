92c3e965f83611c1a93e5243547414589825398c
ol3gatron <olegatron@gmail.com>
2022-05-13 14:36:16 +0300

    added local storage

--- es6.js
+++ es6.js
@@ -76,6 +76,54 @@ class UI {
   }
 }
 
+// Local storage class
+class Store {
+  static getBooks() {
+    let books
+    if(localStorage.getItem("books") === null) {
+      books = []
+    } else {
+      books = JSON.parse(localStorage.getItem("books"))
+    }
+
+    return books
+  }
+
+  static displayBooks() {
+    const books = Store.getBooks()
+
+    books.forEach(function(book) {
+      const ui = new UI
+
+      // Add book to UI
+      ui.addBookToList(book)
+    })
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks()
+
+    books.push(book)
+
+    localStorage.setItem('books', JSON.stringify(books))
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks()
+
+    books.forEach(function(book, index) {
+      if (book.isbn === isbn) {
+        books.splice(index, 1)
+      }
+    })
+
+    localStorage.setItem('books', JSON.stringify(books))
+  }
+}
+
+// DOM Load event
+document.addEventListener("DOMContentLoaded", Store.displayBooks)
+
 // Event listener for add book
 const form = document.querySelector(".book-form")
 form.addEventListener("submit", function(e) {
@@ -101,6 +149,9 @@ form.addEventListener("submit", function(e) {
     // Add book to list
     ui.addBookToList(book);
 
+    // Add to LS
+    Store.addBook(book)
+
     // Clear fiels
     ui.clearFields()
   }
@@ -116,6 +167,9 @@ document.querySelector(".book-list").addEventListener("click", function(e) {
 
     // Delete book
     ui.deleteBook(e.target)
+
+    // Remove from LS
+    Store.removeBook(e.target.parentElement.previousElementSibling.previousElementSibling.textContent)
 
     //Show message
     ui.showAlert("Book removed!", "success")
